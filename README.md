e8eca7b04ff30fc62d619546a0c76128faa4d4d8
Jason Zhang <zjjc123@hotmail.com>
2020-03-27 23:06:44 -0700

    Simple Server
    @daniel-sudz tutorials op

--- README.md
+++
@@ -1,1 +0,0 @@
-# Simple-Nodejs-Server
---
+++ server.js
@@ -0,0 +1,18 @@
+var compression = require('compression');
+const express = require('express');
+
+var app = express();
+module.exports = app;
+
+app.use(compression());
+
+
+var path = require('path');
+app.use(express.static(path.resolve('./public')));
+
+
+app.get('/', async function (req,res) {
+	res.send("Welcome to the https server");
+});
+
+app.listen(80, () => console.log(`Listening on Port 80`))
