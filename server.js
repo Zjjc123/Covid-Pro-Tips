95f2593c8daf17d1ae57c3ed45db106d814d27d0
Daniel Sudzilouski <daniel@sudzilouski.com>
2020-03-29 13:24:58 -0700

    autoupdate

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(express.static(path.resolve('./public')));
 
 
 app.get('/', (req, res) => {
-	res.sendfile('home.html', { root: __dirname + "/public" } );
+	res.sendFile('home.html', { root: __dirname + "/public" } );
 });
 
 app.get('/calculator', (req, res) => {
-	res.sendfile('calculator.html', { root: __dirname + "/public" } );
+	res.sendFile('calculator.html', { root: __dirname + "/public" } );
 });
 
 app.get('/safe-medication-mixing.html', (req, res) => {
-	res.sendfile('medication.html', { root: __dirname + "/public" } );
+	res.sendFile('medication.html', { root: __dirname + "/public" } );
 });
