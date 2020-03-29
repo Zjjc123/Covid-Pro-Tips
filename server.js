5a12548d8c0330af6d41dba07901c16c57b82ce3
Jason Zhang <zjjc123@hotmail.com>
2020-03-29 10:12:27 -0700

    Fix? 2

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(express.static(path.resolve('./public')));
 
 
 app.get('/', (req, res) => {
-	res.sendfile('public.html', { root: __dirname + "/public" } );
+	res.sendfile('home.html', { root: __dirname + "/public" } );
 });
 
 app.get('/calculator', (req, res) => {
-	res.sendfile('public.html', { root: __dirname + "/public" } );
+	res.sendfile('calculator.html', { root: __dirname + "/public" } );
 });
 
 app.get('/safe-medication-mixing.html', (req, res) => {
-	res.sendfile('public.html', { root: __dirname + "/public" } );
+	res.sendfile('medication.html', { root: __dirname + "/public" } );
 });
