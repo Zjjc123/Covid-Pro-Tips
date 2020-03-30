c0d8fd7070714d65c96f78eecb3a4466c2330154
Jason Zhang <zjjc123@hotmail.com>
2020-03-29 22:07:58 -0700

    Async Fetch Test

--- server.js
+++ server.js
@@ -25,58 +25,58 @@ app.get('/medication.html', (req, res) => {
 	res.sendFile('medication.html', { root: __dirname + "/public" });
 });
 
+
+var globalCases = 0;
+var globalRecovered = 0;
+var globalNew = 0;
+var globalDeath = 0;
+
+var usaCases = 0;
+var usaRecovered = 0;
+var usaNew = 0;
+var usaDeath = 0;
+
+const url = "https://covid-193.p.rapidapi.com/statistics";
+
 app.get('/data', (req, res) => {
-
-	var globalCases = 0;
-	var globalRecovered = 0;
-	var globalNew = 0;
-	var globalDeath = 0;
-
-	var usaCases = 0;
-	var usaRecovered = 0;
-	var usaNew = 0;
-	var usaDeath = 0;
-
-
-	const url = "https://covid-193.p.rapidapi.com/statistics";
-	fetch(url, {
-		method: "GET",
-		withCredentials: true,
-		"headers": {
-			"x-rapidapi-host": "covid-193.p.rapidapi.com",
-			"x-rapidapi-key": process.env.API_KEY
-		}
-	})
-		.then(resp => resp.json())
-		.then(function (data) {
-			console.log(data);
-			for (countries in data.response) {
-				if (data.response[countries].country == 'All') {
-					globalCases = data.response[countries].cases.total;
-					globalRecovered = data.response[countries].cases.recovered;
-					globalNew = data.response[countries].cases.new;
-					globalDeath = data.response[countries].deaths.total;
-				}
-				if (data.response[countries].country == 'USA') {
-					usaCases = data.response[countries].cases.total;
-					usaRecovered = data.response[countries].cases.recovered;
-					usaNew = data.response[countries].cases.new;
-					usaDeath = data.response[countries].deaths.total;
-				}
-			}
-
-		})
-
-		.catch(function (error) {
-			console.log(error);
-		});
-
+	getJSON().then(data => setData(data));
 	res.json(
 		[
 			{GlobalCases : globalCases, GlobalRecovered : globalRecovered, GlobalNew : globalNew, GlobalDeath : globalDeath}, 
 			{USACases : usaCases, USARecovered : usaRecovered, USANew : usaNew, USADeath : usaDeath}, 
 		]
 	);
+	
 });
 
+async function getJSON() {
+	let response = await fetch(url, {
+		method: "GET",
+		withCredentials: true,
+		"headers": {
+			"x-rapidapi-host": "covid-193.p.rapidapi.com",
+			"x-rapidapi-key": process.env.API_KEY
+		}
+	});
+	let data = await response.json();
+	return data;
+}
 
+function setData(data)
+{
+	for (countries in data.response) {
+		console.log(data.response[countries]);
+		if (data.response[countries].country == 'All') {
+			globalCases = data.response[countries].cases.total;
+			globalRecovered = data.response[countries].cases.recovered;
+			globalNew = data.response[countries].cases.new;
+			globalDeath = data.response[countries].deaths.total;
+		}
+		if (data.response[countries].country == 'USA') {
+			usaCases = data.response[countries].cases.total;
+			usaRecovered = data.response[countries].cases.recovered;
+			usaNew = data.response[countries].cases.new;
+			usaDeath = data.response[countries].deaths.total;
+		}
+	}
+}
