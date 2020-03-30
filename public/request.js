68f88b7101af137824921fd458980281eee402fe
Jason Zhang <zjjc123@hotmail.com>
2020-03-29 23:29:33 -0700

    Request Fix

--- public/request.js
+++ public/request.js
@@ -3,12 +3,14 @@ var url = "https://www.webcovid-19.live/data";
 
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        myFunction(this.responseText);
+        var parsed = JSON.parse(this.responseText);
+        SetText(parsed);
     }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function myFunction(json) {
+function SetText(json) {
+    console.log(json);
     document.getElementById("GlobalCases").innerHTML = json[0].GlobalCases;
 }
