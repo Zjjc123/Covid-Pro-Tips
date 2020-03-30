c5c29090a520e1555fa1d7494f14ce41738be677
Jason Zhang <zjjc123@hotmail.com>
2020-03-29 20:23:55 -0700

    Test API pulling

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var compression = require('compression');
 var express = require('express');
 
+require('dotenv').config();
+
 var app = express();
 module.exports = app;
 
@@ -11,13 +13,30 @@ app.use(express.static(path.resolve('./public')));
 
 
 app.get('/', (req, res) => {
-	res.sendFile('dashboard.html', { root: __dirname + "/public" } );
+	res.sendFile('dashboard.html', { root: __dirname + "/public" });
 });
 
 app.get('/calculator', (req, res) => {
-	res.sendFile('calculator.html', { root: __dirname + "/public" } );
+	res.sendFile('calculator.html', { root: __dirname + "/public" });
 });
 
-app.get('/safe-medication-mixing.html', (req, res) => {
-	res.sendFile('medication.html', { root: __dirname + "/public" } );
+app.get('/medication.html', (req, res) => {
+	res.sendFile('medication.html', { root: __dirname + "/public" });
 });
+
+
+const url = "https://covid-193.p.rapidapi.com/countries";
+fetch(url, {
+	method: "GET",
+	withCredentials: true,
+	headers: {
+		"X-Auth-Token": process.env.API_KEY,
+	}
+})
+	.then(resp => resp.json())
+	.then(function (data) {
+		console.log(data);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
