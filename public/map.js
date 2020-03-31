af0fa6fd9f5a58914db8927455307be6a56951f5
Rohan Krishnan <rohankrishnan@live.com>
2020-03-30 17:24:43 -0700

    exported non functional map

---
+++ public/map.js
@@ -0,0 +1,32 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "https://www.webcovid-19.live/all";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var parsed = JSON.parse(this.responseText);
+        SetMap(parsed);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function SetMap(json) {
+  var dataSet = [];
+    // for (i = 0; i < json.length; i++) {
+    //     dataSet.push( {'id': json[i].country, 'value': json[i].cases.total} );
+    // }
+
+  //
+  var map = anychart.map();
+  map.geoData(anychart.maps.world);
+
+  // set the series
+  var series = map.choropleth(dataSet);
+
+  // disable labels
+  series.labels(true);
+
+  // set the container
+  map.container('container');
+  map.draw();
+});
